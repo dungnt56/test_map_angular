8ebfc8fd3592287cf8dc57f0dfa586b5ef87403a
DungNt <tandung.nguyen@migitek.com>
2024-10-07 11:05:00 +0700

    Leaflet map with google

--- src/leaflet-map/leaflet-map.component.ts
+++ src/leaflet-map/leaflet-map.component.ts
@@ -1,5 +1,7 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import * as L from 'leaflet';
+import { Language } from '@maptiler/sdk';
+import * as maptiler from '@maptiler/sdk';
 @Component({
   selector: 'app-leaflet-map',
   standalone: true,
@@ -10,41 +12,42 @@ import * as L from 'leaflet';
 export class LeafletMapComponent implements OnInit, AfterViewInit {
   private map: any;
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [16.047079, 108.206230], // Toạ độ tại Đà Nẵng, Việt Nam
-      zoom: 5
-    });
+  // private initMap(): void {
+  //   // Khởi tạo bản đồ với trung tâm tại Việt Nam
+  //   this.map = L.map('map').setView([21.0285, 105.8542], 13);
+  //
+  //   // Thêm tile layer với ngôn ngữ tiếng Việt (tùy chọn bản đồ có hỗ trợ tiếng Việt)
+  //   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     maxZoom: 19,
+  //     attribution: '© OpenStreetMap'
+  //   }).addTo(this.map);
+  //
+  //   // Thêm marker với popup hiển thị tiếng Việt
+  //   const marker = L.marker([21.0285, 105.8542]).addTo(this.map);
+  //   marker.bindPopup("<b>Chào mừng!</b><br>Đây là Hà Nội, Việt Nam.").openPopup();
+  // }
 
-    // Thêm tile từ MapTiler với API Key
-    L.tileLayer('https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=MoSM18XqoOPW3oCJdooM&language=en', {
-      attribution: '&copy; <a href="https://www.maptiler.com/copyright/" target="_blank">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-      maxZoom: 19
-    }).addTo(this.map);
+  private initMap(): void {
+    this.map = L.map('map').setView([21.027, 105.854], 12);
+    const street = L.tileLayer(
+      'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=vi', {
+        minZoom: 4,
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+        noWrap: true
+      }
+    )
+    street.addTo(this.map);
   }
 
   ngOnInit(): void {
     this.initMap();
   }
-    // this.map = L.map('map').setView([21.0285, 105.8542], 13);
-    //
-    // // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    // //   maxZoom: 19,
-    // //   attribution: '© OpenStreetMap'
-    // // }).addTo(this.map);
-    // L.tileLayer('https://api.maptiler.com/maps/basic-vi/{z}/{x}/{y}.png?key=MoSM18XqoOPW3oCJdooM', {
-    //   maxZoom: 19,
-    //   attribution: '© MapTiler © OpenStreetMap contributors'
-    // }).addTo(this.map);
-    //
-    // const marker = L.marker([21.0285, 105.8542]).addTo(this.map);
-    // marker.bindPopup("<b>Chào mừng!</b><br>Đây là Hà Nội, Việt Nam.").openPopup();
-  // }
-  // constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   // ngOnInit(): void { }
 
   ngAfterViewInit(): void {
-    this.initMap();
+    // this.initMap();
   }
 }
