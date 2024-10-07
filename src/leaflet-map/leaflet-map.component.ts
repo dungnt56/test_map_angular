157c3c8d3da7477ae8486f7cf6c4185e11735418
DungNt <tandung.nguyen@migitek.com>
2024-10-07 11:22:17 +0700

    Leaflet map service

--- src/leaflet-map/leaflet-map.component.ts
+++ src/leaflet-map/leaflet-map.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 import { Language } from '@maptiler/sdk';
 import * as maptiler from '@maptiler/sdk';
+import {MapService} from "./map.service";
 @Component({
   selector: 'app-leaflet-map',
   standalone: true,
@@ -10,44 +11,21 @@ import * as maptiler from '@maptiler/sdk';
   styleUrl: './leaflet-map.component.css'
 })
 export class LeafletMapComponent implements OnInit, AfterViewInit {
-  private map: any;
-
-  // private initMap(): void {
-  //   // Khởi tạo bản đồ với trung tâm tại Việt Nam
-  //   this.map = L.map('map').setView([21.0285, 105.8542], 13);
-  //
-  //   // Thêm tile layer với ngôn ngữ tiếng Việt (tùy chọn bản đồ có hỗ trợ tiếng Việt)
-  //   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //     maxZoom: 19,
-  //     attribution: '© OpenStreetMap'
-  //   }).addTo(this.map);
-  //
-  //   // Thêm marker với popup hiển thị tiếng Việt
-  //   const marker = L.marker([21.0285, 105.8542]).addTo(this.map);
-  //   marker.bindPopup("<b>Chào mừng!</b><br>Đây là Hà Nội, Việt Nam.").openPopup();
-  // }
-
-  private initMap(): void {
-    this.map = L.map('map').setView([21.027, 105.854], 12);
-    const street = L.tileLayer(
-      'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=vi', {
-        minZoom: 4,
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-        noWrap: true
-      }
-    )
-    street.addTo(this.map);
-  }
+  private map: L.Map | undefined;
 
   ngOnInit(): void {
     this.initMap();
   }
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef,
+              private mapService: MapService) { }
 
-  // ngOnInit(): void { }
 
-  ngAfterViewInit(): void {
-    // this.initMap();
+
+  private initMap(): void {
+    this.map = L.map('map').setView([21.027, 105.854], 12);
+    this.mapService.createTileLayer().addTo(this.map);
+  }
+
+  ngAfterViewInit() {
   }
 }
---
+++ src/leaflet-map/map.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+  private map: L.Map | undefined;
+
+  constructor() {}
+
+  createTileLayer(): L.TileLayer {
+    return L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=vi', {
+      minZoom: 4,
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      noWrap: true,
+    });
+  }
+  // Khởi tạo bản đồ
+  initMap(containerId: string, center: [number, number], zoom: number): L.Map {
+    this.map = L.map(containerId).setView(center, zoom);
+    return this.map;
+  }
+
+  // Thêm lớp bản đồ
+  addTileLayer(url: string, options: any): void {
+    if (this.map) {
+      L.tileLayer(url, options).addTo(this.map);
+    }
+  }
+
+  // Thêm sự kiện click
+  addClickEvent(callback: (e: L.LeafletMouseEvent) => void): void {
+    if (this.map) {
+      this.map.on('click', callback);
+    }
+  }
+
+  // Thêm marker
+  addMarker(latlng: [number, number], popupContent?: string): L.Marker {
+    if (this.map) {
+      const marker = L.marker(latlng).addTo(this.map);
+      if (popupContent) {
+        marker.bindPopup(popupContent);
+      }
+      return marker;
+    }
+    throw new Error('Map is not initialized');
+  }
+
+  setMap(map: L.Map) {
+    this.map = map;
+  }
+  // Lấy đối tượng map
+  getMap(): L.Map | undefined {
+    return this.map;
+  }
+  addPolygon(geoJsonData: any, style?: L.PathOptions) {
+    if (!this.map) {
+      throw new Error('Map is not initialized');
+    }
+    const polygon = L.geoJSON(geoJsonData, { style }).addTo(this.map);
+    this.map.fitBounds(polygon.getBounds());
+  }
+}
